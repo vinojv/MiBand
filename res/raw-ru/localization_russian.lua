5f77279cfc9515eac4fb5ba76ea2e4cd457389e0
Zenk <zenk@list.ru>
2014-09-18 18:26:44 +0400

    initial release

---
+++ res/raw-ru/localization_russian.lua
@@ -0,0 +1,73 @@
+-------------------------
+-- Rusian Localization String resources
+-- MIUI Russia miui.su (2014)
+-- Herbert dai
+-- 20140731
+-------------------------
+localization_Russian_table = {
+    calories_table={
+
+    },
+
+    ok='OK',
+
+    hour='ч.',
+    minute='мин.',
+    month='мес.',
+    day='дн.',
+    step="шаг",
+    km='КМ',
+    minute_format='%d мин.',
+    hour_minute_format='%d hrs %d мин.',
+    hour_format='%d ч.',
+
+    welcome_use="Привет, я твой Mi Band.",
+    last_week_walked_format='За прошлый месяц пройдено шагов: %d.',
+    last_week_walked_info_format='Пройдено %d км, сожжено %d кал.',
+    last_month_walked_format='За прошлый месяц пройдено шагов: %d.',
+    last_month_walked_info_format='Пройдено %d км, сожжено %d кал.',
+
+    get_over_format=", about %d gram of fat.",
+    click_to_get_help="Find out more to do with Mi Band.",
+    take_a_walk="Take a walk with Mi Band.",
+    take_a_walk_info="Mi Band will track your activities and help you to be healthier.",
+    unlock_hint="Unlock phone with your Mi Band",
+    unlock_hint_info="say Goodbye to the old times.",
+    no_data_hint_0="Keep moving, make sports a part of your life.",
+    no_data_hint_1="Want to stay happy? Just run.",
+    no_data_hint_2="Cardio workout will make you light and fresh every day! ",
+    no_data_hint_3="A normal adult spends 15%-20% of total sleep time in deep sleep stage. ",
+    not_binded_hint="You haven’t linked any Mi Band to this account, please link one now. ",
+    new_record_info="WoW! You just broke a record in walking.",
+    new_record_format="New record, %d steps, %s %d",
+    today_goal_reached="Congrats! You have achieved your goal today. ",
+    week_continue_reach_goal="You have achieved your goal every day for an entire week! What an awesome week!",
+    challenge_format="You have achieved your goal for %d days!",
+    personal_best_format="Your highest record is %d days!",
+    active_time_format_0="%d мин.",
+    active_time_format_1="%d ч.",
+    active_time_format_2="%d ч. %d мин.",
+    activie_run_format_0="Ran %s. Good job!",
+    activie_run_format_1="Ran %s. You are doing fantastic!",
+    activie_run_format_2="Ran %s, Incredible endurance!",
+    activie_run_consumed="Сожжено %d кал. %s",
+
+    get_distance_format=", a total of %d m.",
+    activity_walk_format="%s Walked %d steps %s",
+    activity_walk_consumed_format="Burned %d cal %s",
+
+    activity_activity_format="%s %s work out %s",
+    last_night_sleeped_good_format="Last night, you slept for %s. What a great sleep!",
+    last_night_sleeped_normal_format="Last night, you slept for %s.",
+    deep_sleep_format="Deep sleep stage was %s.",
+
+    battery_low_info="Low in battery, please charge your Mi Band.",
+    battery_very_low_info="Connect to power",
+
+    cannot_find_bracelet="Не удалось найти ваш Mi Band?",
+    cannot_find_bracelet_info="Please make sure your Mi Band has power and stays nearby your phone.",
+
+    challenge_to_get="%s days from making a new record!",
+    record_reach_max="You have hit your highest record!",
+    new_record_born="Новый рекорд!",
+ }
