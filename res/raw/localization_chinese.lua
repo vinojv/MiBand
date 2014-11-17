c1cae6d64269c748e05b6854e949fc01db5a9343
Zenk <zenk@list.ru>
2014-11-17 19:43:05 +0300

    1.1.712_264 eng/rus

--- res/raw/localization_chinese.lua
+++ res/raw/localization_chinese.lua
@@ -1,7 +1,8 @@
 -------------------------
--- Rusian Localization String resources
+-- Russian Localization String resources
 -- MIUI Russia miui.su (2014)
--- 20140919
+-- Zenk
+-- 20140731
 -------------------------
 localization_Chinese_table = {
     calories_table={
@@ -14,8 +15,8 @@ localization_Chinese_table = {
     minute='мин.',
     month='мес.',
     day='дн.',
-    step="шаг",
-    km='км',
+    step="ш.",
+    km='км.',
     minute_format='%d мин.',
     hour_minute_format='%d ч. %d мин.',
     hour_format='%d ч.',
@@ -29,20 +30,35 @@ localization_Chinese_table = {
     get_over_format=", около %d гр. жира.",
     click_to_get_help="Узнайте обо мне побольше.",
     take_a_walk="Прогуляйтесь с Mi Band.",
-    take_a_walk_info="Mi Band будет отслеживать вашу активность и поможет сохранить здоровье.",
+    take_a_walk_info="Mi Band будет отслеживать вашу активность в течение дня.",
     unlock_hint="Разблокируйте телефон своим Mi Band",
     unlock_hint_info="Попрощайтесь со старыми временами",
     no_data_hint_0="Двигайтесь, сделайте спорт частью своей жизни.",
     no_data_hint_1="Хотите быть счастливым? Просто бегите.",
     no_data_hint_2="Кардио тренировки сделают тебя бодрым и энергичным на весь день! ",
-    no_data_hint_3="Взрозлый человек проводит в стадии глубокого сна 15%-20% от общего сна. ",
+    no_data_hint_3="Взрослый человек проводит в стадии глубокого сна 15%-20% от общего сна. ",
     not_binded_hint="Вы не привязали ни один Mi Band к этому аккаунту, самое время сделать это сейчас. ",
-    new_record_info="Вау! Вы побили свой рекорд по ходьбе.",
-    new_record_format="Новый рекорд, шагов: %d, %s %d",
+    new_record_info="Вы побили свой рекорд по ходьбе!",
+    new_record_format="Шагов: %d, %s %d",
     today_goal_reached="Поздравляю! Вы сегодня достигли своей цели. ",
     week_continue_reach_goal="Вы достигали своей цели на протяжении недели! Отличная неделя!",
-    challenge_format="Вы достигли цели за %d дн.!",
+	
+    challenge_format="Вы достигли цели за %d дн.!",	
     personal_best_format="Лучший рекорд за %d дн.!",
+    continue_with_skips_title="You didn't hit your goal yesterday. Let's call it a Lazy Day ;)",
+    continue_with_skips_title_today="You didn't hit your goal today. Let's call it a Lazy Day ;)",
+    personal_best_with_skips_format="You have %d Lazy Days left ;)",
+    continue_used_n_skips_format="You've hit your goal for %d days in a row! You still have %d Lazy Days",
+    continue_used_0_skips_format="You've hit your goal for %d days in a row! You have no Lazy Days left.",
+    personal_best_on_skip_day_title="%d day streak! You've earned a Lazy Day!",
+    personal_best_on_skip_day_info="After every 7-day streak you earn a Lazy Day. You can use Lazy Days to cover days when you don't hit your goal.",
+    manual_lazy_title="You didn't hit your goal today.",
+    manual_lazy_subtitle="Your streak will continue if you mark today as a Lazy Day.",
+    manual_lazy_title_used="Today is a Lazy Day.",
+    manual_lazy_subtitle_used="You can earn more Lazy Days by breaking personal records.",
+    manual_fail_ytd_title = "You ended your streak yesterday.",
+    manual_fail_ytd_subtitle = "Streak was %d long.",
+	
     active_time_format_0="%d мин.",
     active_time_format_1="%d ч.",
     active_time_format_2="%d ч. %d мин.",
@@ -50,23 +66,64 @@ localization_Chinese_table = {
     activie_run_format_1="Бег %s. Фантастика!",
     activie_run_format_2="Бег %s, Невероятная выносливость!",
     activie_run_consumed="Сожжено %d ккал. %s",
-
+	
     get_distance_format=", всего %d м.",
     activity_walk_format="%s Пройдено %d шагов %s",
     activity_walk_consumed_format="Сожжено %d ккал. %s",
-
+	
+    activity_rope_skipping_format="%s jumped rope %d times",
+    activity_situp_format="%s приседаний: %d",
+    activity_consumed_format="Сожжено ккал: %d %s",
     activity_activity_format="%s %s тренировка %s",
+    activity_activity_format_run="%s Worked out for %s, ran %s",
     last_night_sleeped_good_format="Прошлой ночью ты спал %s Чудесный сон!",
     last_night_sleeped_normal_format="Прошлой ночью ты спал %s.",
     deep_sleep_format="В стадии глубокого сна %s.",
 
-    battery_low_info="Низкий заряд батареи.",
+    battery_low_info="Низкий заряд батареи",
     battery_very_low_info="Зарядите Mi Band",
 
     cannot_find_bracelet="Не удалось найти ваш Mi Band?",
     cannot_find_bracelet_info="Убедитесь, что Mi Band находится близко к телефону.",
-
     challenge_to_get="%s дню с момента нового рекорда!",
     record_reach_max="Вы побили свой рекорд!",
     new_record_born="Новый рекорд!",
+
+    --=================== Game =====================
+    game_register="国庆七天日行万步, 赢取小米手机4",
+    game_not_register_info="分享微博赢取微博大奖",
+    game_register_info="已成功报名, 倒计时%s天",
+    game_playing="国庆七天日行万步, 第%s天挑战中",
+    game_playing_done="国庆七天日行万步, 第%s天达成",
+    game_playing_lastday="最后一天即将完成日行万步挑战, 加油",
+    game_playing_lastday_done="恭喜, 国庆七天日行万步挑战成功",
+    game_playing_bonus_info="中将结果将在%s月%s日揭晓",
+    click_to_show_result="点击查看活动进展",
+    game_fail_title_ytd="很遗憾, 昨天未达成10000步",
+    game_fail_title="很遗憾, %s月%s日未达成10000步",
+    game_failed_title="国庆七天日行万步",
+    game_fail_info="继续关注活动, 分享微博赢取微博大奖",
+
+    game_bonus_hit="恭喜你成为“国庆七天日行万步”获奖者",
+    game_bonus_hit_info="点击查看中奖结果",
+    game_bonus_miss="国庆七天日行万步, 中奖结果已经揭晓",
+    game_bonus_miss_info="快去看看哪些人中奖了吧",
+    game_bonus_open="中奖结果将在%s月%s日揭晓, 敬请关注",
+    game_bonus_open_tomorrow="中奖结果将在明天揭晓, 敬请关注",
+    game_bonus_open_today="中奖结果将在今天揭晓, 敬请关注",
+
+    share_to_content_for_event="“国庆七天日行万步得小米手机4”活动火热报名中，没有手环? 转发此微博即有机会赢取手环F码，点击链接查看活动详情。",
+    game_share_to_registered="我报名参加了“日行万步得小米手机4”的活动，你愿意站在一旁默默祝福我还是一起来参加呢？离活动开始只剩%s天了, 快来报名吧",
+    game_share_to_finished="今天10000步目标顺利完成",
+    game_share_to_not_finished="向着10000步前进",
+    game_share_to_failed="没想到呀，我居然挑战失败了",
+    game_share_to_success="挑战成功！一共走了%s步",
+    game_share_to_hit_bonus="一个不小心中奖了，快来围观给我点赞吧",
+    game_share_to_not_hit_bonus="没有抽中，就当是给下次中奖攒人品了",
+    less_than_one="小于1",
+
+    ---====== Weather tips ===============
+    weather_tips_title_0 = "Сильное загрязнение, лучше останьтесь дома.",
+    weather_tips_title_1 = "Сильное загрязнение, лучше останьтесь дома.",
+    weather_tips_info = "Приседания или занятия со скакалкой - отличные упражнения в помещении!",
  }
